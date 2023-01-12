5d4f91616895402d5b85f46509f7839f7fed925c
VitalGa <vitalga10@gmail.com>
2023-01-12 09:14:57 +0300

    replase readlineSync to evenGame

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
-import readlineSync from 'readline-sync';
+// import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  // const name = readlineSync.question('May I have your name? ');
+  // console.log(`Hello, ${name}! \nAnswer "yes" if the number is even, otherwise answer "no".`);
+};
 
-export var userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+export default greeting;
