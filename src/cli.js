abd8a9c083eb881eacb77ff96e20c65fe34ab010
VitalGa <vitalga10@gmail.com>
2023-01-13 07:48:01 +0300

    changed cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-// import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  // const name = readlineSync.question('May I have your name? ');
-  // console.log(`Hello, ${name}! \nAnswer "yes" if the number is even, otherwise answer "no".`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default greeting;
