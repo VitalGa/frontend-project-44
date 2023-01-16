78001db4db3935eab7b338cd110c9f69b146e366
VitalGa <vitalga10@gmail.com>
2023-01-16 20:25:23 +0300

    coding

--- src/games/gcdGame.js
+++ src/games/gcdGame.js
@@ -1,9 +1,26 @@
 import engineForAllGames from '../index.js';
 import getRandomInt from '../getRandomInt.js';
 
-const gameRulesGcd = 'What is the result of the expression?';
+const gameRulesGcd = 'Find the greatest common divisor of given numbers.';
 
+const expressionGcd = (n1, n2) => {
+  if (n2 === 0) {
+    return n1;
+  }
 
-const gcdGame = () => engineForAllGames(gameRulesGcd, ????);
+  return expressionGcd(n2, n1 % n2);
+};
+
+const dataForRoundgcd = () => {
+  const num1 = getRandomInt(1, 25);
+  const num2 = getRandomInt(1, 25);
+
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(expressionGcd(num1, num2));
+
+  return (question, correctAnswer);
+};
+
+const gcdGame = () => engineForAllGames(gameRulesGcd, dataForRoundgcd);
 
 export default gcdGame;
