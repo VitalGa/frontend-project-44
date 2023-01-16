ba1312ffde361f6e2222039a7a2e8180ee2dd52c
VitalGa <vitalga10@gmail.com>
2023-01-16 20:10:18 +0300

    codding

---
+++ src/games/gcdGame.js
@@ -0,0 +1,9 @@
+import engineForAllGames from '../index.js';
+import getRandomInt from '../getRandomInt.js';
+
+const gameRulesGcd = 'What is the result of the expression?';
+
+
+const gcdGame = () => engineForAllGames(gameRulesGcd, ????);
+
+export default gcdGame;
