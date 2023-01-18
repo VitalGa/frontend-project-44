4f18ebd5dd5ef915ff43fb776ee4065fefccee6d
VitalGa <vitalga10@gmail.com>
2023-01-18 15:26:18 +0300

    codding

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -3,6 +3,27 @@ import getRandomInt from '../getRandomInt.js';
 
 const gameRulesProgression = 'What number is missing in the progression?';
 
+const generateProgression = (firstItem, step, Length) => {
+  const array = [];
+  for (let i = 0; i < Length; i += 1) {
+    const result = (firstItem + (step * i));
+    array.push(result);
+  }
+  return array;
+};
+
+const dataForRoundProgression = () => {
+  const first = getRandomInt(1, 10);
+  const step = getRandomInt(1, 10);
+  const length = getRandomInt(5, 10);
+  const progression = generateProgression(first, step, length);
+  const randIndex = Math.floor(Math.random() * progression.length);
+
+  const correctAnswer = `${progression[randIndex]}`;
+  progression[randIndex] = '..';
+  const question = progression.join(' ');
+  return [question, correctAnswer];
+};
 
 const progressionGame = () => engineForAllGames(gameRulesProgression, dataForRoundProgression);
 
