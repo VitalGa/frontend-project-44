4b66106289d9c6bea52e39ba2dcbf70dc8428368
VitalGa <vitalga10@gmail.com>
2023-01-19 20:24:58 +0300

    create file

---
+++ src/games/primeGame.js
@@ -0,0 +1,24 @@
+import engineForAllGames from '../index.js';
+import getRandomInt from '../getRandomInt.js';
+
+const gameRulesPrime = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+
+
+
+
+
+
+
+
+
+
+
+onst dataForRoundPrime = () => {
+     
+  };
+
+
+const primeGame = () => engineForAllGames(gameRulesPrime, dataForRoundPrime);
+
+export default primeGame;
