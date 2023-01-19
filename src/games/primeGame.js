f9c4fe8d6f3a3fdeb5cdfa52fcf89234d0a602e8
VitalGa <vitalga10@gmail.com>
2023-01-19 20:40:21 +0300

    codding

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -3,21 +3,27 @@ import getRandomInt from '../getRandomInt.js';
 
 const gameRulesPrime = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
+const primeNumber = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
+const dataForRoundPrime = () => {
+  const number = getRandomInt(1, 50);
 
+  const question = `${number}`;
 
+  const correctAnswer = primeNumber(number) ? 'yes' : 'no';
 
-
-
-
-
-
-
-
-onst dataForRoundPrime = () => {
-     
-  };
-
+  return [question, correctAnswer];
+};
 
 const primeGame = () => engineForAllGames(gameRulesPrime, dataForRoundPrime);
 
